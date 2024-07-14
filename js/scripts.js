0c1d590767f009340b678026001224f9aaba24af
Samuel Muigai <127452959+Muigai-Kiongo@users.noreply.github.com>
2024-07-14 15:16:05 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+//identifying major page content
+const login = document.getElementById('login')
+const register = document.getElementById('register')
+const home = document.querySelector('main')
+const booking = document.getElementById('booking-form')
+
+//additional variables
+const btnBook = document.querySelector(".btn")
+const btnLogin = document.getElementById('btnLogin')
+const btnRegister = document.getElementById('btnRegister')
+
+
+// onload default
+
+
+//instances
+btnBook.onclick = () => {
+    home.style.display = 'none'
+    booking.style.display = 'block'
+}
+
+btnLogin.onclick = () => {
+    home.style.display = 'none'
+    register.style.display = 'none'
+    login.style.display = 'block'
+
+}
+
+btnRegister.onclick = () => {
+    home.style.display = 'none'
+    login.style.display = 'none'
+    register.style.display = 'block'
+}
+
+document.querySelectorAll('a')
+
+//initializing booking process
+
+
